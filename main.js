76228ccf2638215bca66d841803f972905154767
Orçun Tuna <turboindirblog@gmail.com>
2019-05-30 20:54:06 +0300

    add: mainWindow

---
+++ main.js
@@ -0,0 +1,47 @@
+const electron = require('electron')
+const url = require("url")
+const path = require("path")
+const app = electron.app
+
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow() {
+
+  mainWindow = new BrowserWindow({
+    title: "Electro Pass Reminder",
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "template/mainWindow.html"),
+    protocol: "file:",
+    slashes: true
+  }));
+
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
+
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
